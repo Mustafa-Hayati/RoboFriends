4930bc4a39dc3c49bcb8877f9ed3768261f0332c
Mustafa-Hayati <mustafa.hayati1992@gmail.com>
2019-11-29 14:32:09 +0330

    minor changes to improve the cards component

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -2,15 +2,11 @@ import React from "react";
 import Card from "./Card/Card";
 import { robots } from "../../robots";
 
-// function randomNum(limit) {
-//   return Math.floor(Math.random() * limit) + 1;
-// }
-
 const Cards = props => (
   <div>
-    {robots.map(({ email, name, id }) => {
-      return <Card id={id} key={id} name={name} email={email} />;
-    })}
+    {robots.map(({ email, name, id }) => (
+      <Card id={id} key={id} name={name} email={email} />
+    ))}
   </div>
 );
 
