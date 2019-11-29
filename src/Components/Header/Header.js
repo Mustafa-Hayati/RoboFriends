b31d87e3fe71f578c291d25a5b1ee29eca974afe
Mustafa-Hayati <mustafa.hayati1992@gmail.com>
2019-11-29 16:35:11 +0330

    added SEGA font to the header and changed the background of body

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
 import SearchBox from "./Searchbox/Searchbox";
+import "./Header.css";
 
 const Header = ({ searchChange }) => (
   <header>
-    <h1>RoboFriends</h1>
+    <h1 className="f1">RoboFriends</h1>
     <SearchBox change={searchChange} />
   </header>
 );
