9b6255ea62b3088d7fcf50ec803145544839dbb5
Mustafa-Hayati <mustafa.hayati1992@gmail.com>
2019-11-29 20:04:39 +0330

    Updates

---
+++ precache-manifest.e8842c750609f6248129654527e28cb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aff9afc7538ec017b035d9f54443697",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "332871375e99282f11a6",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "05b917469885f043008a",
+    "url": "/RoboFriends/static/css/main.2172470e.chunk.css"
+  },
+  {
+    "revision": "332871375e99282f11a6",
+    "url": "/RoboFriends/static/js/2.cf0e57c7.chunk.js"
+  },
+  {
+    "revision": "05b917469885f043008a",
+    "url": "/RoboFriends/static/js/main.fd6f85c6.chunk.js"
+  },
+  {
+    "revision": "33edd37477320a27eb83",
+    "url": "/RoboFriends/static/js/runtime-main.34389f62.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
